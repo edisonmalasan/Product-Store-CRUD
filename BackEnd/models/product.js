897e30da581ef2a9fee44dcf3ca0ac7a67f3f827
Edison Malasan <2245504@slu.edu.ph>
2025-01-20 21:59:33 +0800

    added product schema

--- BackEnd/models/product.js
+++ BackEnd/models/product.js
@@ -13,7 +13,11 @@ const productSchema = new mongoose.Schema({
     image: {
         type: String,
         required: true,
-    }
+    },
 }, {
     timestamps: true
 });
+
+const Product = mongoose.model('Product', productSchema);
+
+export 
