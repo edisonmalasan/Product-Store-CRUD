d92d28848f22f29d012a1872a01e4b790ee26f0d
Edison Malasan <2245504@slu.edu.ph>
2025-01-20 21:39:40 +0800

    added db.js file

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,7 +1,15 @@
 import express from "express"; // add the "type": "module" in package to use this import 
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
+
+app.get("/products", (req, res) => {});
+
+console.log(process.env.MONGO_URI);
 
 app.listen(5000, () => {
     console.log("Server started at http://localhost:5000")
 })
+
