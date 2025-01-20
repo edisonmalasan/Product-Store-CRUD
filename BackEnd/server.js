258aa445302edd3fd32f95b44a4a662103d54771
Edison Malasan <2245504@slu.edu.ph>
2025-01-20 23:24:47 +0800

    some changes

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -13,14 +13,14 @@ app.post("/api/products", async (req, res) => {
     const product = req.body; // user will send this data to use it u need to use express.json
 
     if(!product.name || !product.price || !product.image) {
-        return res.status(400).json({ success: false, message: "Please Provide All Fields"}); // 400 status code if client error
+        return res.status(400).json({ success: false, message: "Please provide all fields"}); // 400 status code if client error
     }
 
-    const newProduct = new Product(product)
+    const newProduct = new Product(product);
 
     try {
         await newProduct.save();
-        req.status(200).json({success: true, data: newProduct});
+        req.status(201).json({success: true, data: newProduct});
     } catch (error) {
         console.error("Error in creating product:", error.message)
         return res.status(500).json({success: false, message: "Server Error" }); // 500 status code if server error
