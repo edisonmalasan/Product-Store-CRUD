3d3b478064535df95be7da64461ed2305139b455
Eruki <malasanedison1@gmail.com>
2025-01-21 23:03:59 +0800

    added product deleted by id

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -27,13 +27,27 @@ app.post("/api/products", async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+
+app.delete("/api/products/:id", async (req,res) => {
+    const {id} = req.params
+    
+    try {
+        await Product.findByIdAndDelete(id);
+        res.status(200).json({success: true, message: "Product deleted successfully"});
+    } catch (error) {
+        console.error("Error in deleting product:", error.message);
+        res.status(500).json({success: false, message: "Server Error"});
+    }
+});
+
+
+app.listen(5000, () => {
     connectDB();
     console.log("Server started at http://localhost:5000")
-}). on("error", (error) => {
+}).on("error", (error) => {
     console.error("Error in starting server:", error.message);
     process.exit(1);
 }); // process code 1 code means exit with failure, 0 means success
+
 
     
