620bf0623c46d168b5183236a9dc0f4154c1964b
Edison Malasan <2245504@slu.edu.ph>
2025-01-21 08:28:58 +0800

    added a function where if port 5000 is not avail then it listen to another port

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -28,8 +28,10 @@ app.post("/api/products", async (req, res) => {
 });
 
 
-app.listen(5000, () => {
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
     connectDB();
     console.log("Server started at http://localhost:5000")
 })
 
+    
