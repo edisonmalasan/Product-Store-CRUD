bfcabd5d13683f2694d02e217cebb6f9526b418c
Eruki <malasanedison1@gmail.com>
2025-01-25 22:14:34 +0800

    added createProduct from product controller

--- BackEnd/controller/product.controller.js
+++ BackEnd/controller/product.controller.js
@@ -9,3 +9,22 @@ export const getProducts = async (req, res) => {
         res.status(500).json({success: false, message: "Server Error" }); // 500 status code if server error
     }
 };
+
+export const createProduct = async (req, res) => {
+    const product = req.body; // user will send this data to use it u need to use express.json
+
+    if(!product.name || !product.price || !product.image) {
+        return res.status(400).json({ success: false, message: "Please provide all fields"}); // 400 status code if client error
+    }
+
+    const newProduct = new Product(product);
+
+    try {
+        await newProduct.save();
+        return res.status(201).json({success: true, data: newProduct});
+    } catch (error) {
+        console.error("Error in creating product:", error.message)
+        return res.status(500).json({success: false, message: "Server Error" }); // 500 status code if server error
+    }
+};
+
