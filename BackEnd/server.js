b467b17a9451e025b5d67beb1fdafeaf4e2ed64d
Eruki <malasanedison1@gmail.com>
2025-01-25 22:48:39 +0800

    added port number from environment variable

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -7,13 +7,15 @@ dotenv.config();
 
 const app = express();
 
+const PORT = process.env.PORT || 5000;
+
 app.use(express.json()); // middleware that allows us to accept JSON data in the req.body
 
 app.use("/api/products", productRoutes);
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
     connectDB();
-    console.log("Server started at http://localhost:5000")
+    console.log("Server started at http://localhost:"+ PORT);
 }).on("error", (error) => {
     console.error("Error in starting server:", error.message);
     process.exit(1);
