441ea8baf1d38463d45b719f601e766ef43356c5
Eruki <malasanedison1@gmail.com>
2025-01-26 20:09:37 +0800

    added text in div

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,18 +1,20 @@
+import { Box } from "@chakra-ui/react"
+import { Route, Routes } from "react-router-dom"
 
-import { Routes } from "react-router-dom"
+import Homepage from "./pages/Homepage"
+import CreatePage from "./pages/CreatePage"
+import Navbar from "./components/Navbar"
 
 function App() {
-
   return (
-    <>
       <Box minH={"100vh"}>
-        {Navbar}
+        <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Homepage />} />
+          <Route path="/create" element={<CreatePage />} />
         </Routes>
       </Box>
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
