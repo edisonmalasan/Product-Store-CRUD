8608a2f8172c14e7bf5d6b153614eb47b0767046
Eruki <malasanedison1@gmail.com>
2025-01-27 21:23:07 +0800

    added container, flex and text styling in navbar jsx

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,9 +1,9 @@
-import { Box } from "@chakra-ui/react"
-import { Route, Routes } from "react-router-dom"
+import { Box } from "@chakra-ui/react";
+import { Route, Routes } from "react-router-dom";
 
-import Homepage from "./pages/Homepage"
-import CreatePage from "./pages/CreatePage"
-import Navbar from "./components/Navbar"
+import Homepage from "./pages/Homepage";
+import CreatePage from "./pages/CreatePage";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
--- FrontEnd/src/components/Navbar.jsx
+++ FrontEnd/src/components/Navbar.jsx
@@ -1,10 +1,27 @@
+import { Container } from '@chakra-ui/react'
 import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
-    </div>
+    <Container maxW={"1140px"} px={4}>
+      <Flex>
+        h={16}
+        alignItems={"center"}
+        justifyContent={"space-between"}
+        flexDirection={{ base: "column", md: "row" }}
+
+        <Text
+        fontSize = {{ base: "22", sm: "28" }}
+        fontWeight={"bold"}
+        textTransform={"uppercase"}
+        textAlign={"center"}
+        bgGradient={"linear(to-r, cyan.400,blue.500)"}
+        bgClip={"text"}
+        >
+          <Link to={"/"}> Product Store</Link>
+        </Text>
+      </Flex>
+    </Container>
   )
 }
 
