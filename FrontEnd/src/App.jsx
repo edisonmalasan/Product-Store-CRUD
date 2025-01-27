ae1cc2b6c212b85c2503d9cf77765b23e10cad29
Eruki <malasanedison1@gmail.com>
2025-01-27 22:48:15 +0800

    added useColorModeValue for app layout

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -7,7 +7,7 @@ import Navbar from "./components/Navbar";
 
 function App() {
   return (
-      <Box minH={"100vh"}>
+      <Box minH={"100vh"} bg={useColorModeValue("gray.100", "gray.900")}>
         <Navbar />
         <Routes>
           <Route path="/" element={<Homepage />} />
--- FrontEnd/src/components/Navbar.jsx
+++ FrontEnd/src/components/Navbar.jsx
@@ -1,12 +1,12 @@
 import { PlusSquareIcon } from '@chakra-ui/icons';
-import { Button, Container, Flex, HStack, Text, useColorMode } from '@chakra-ui/react';
+import { Button, Container, Flex, HStack, Text, useColorMode, useColorModeValue } from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   const {colorMode, toggleColorMode } = useColorMode();  
 
   return (
-    <Container maxW={"1140px"} px={4}>
+    <Container maxW={"1140px"} px={4} >
       <Flex         
         h={16}
         alignItems={"center"}
@@ -35,7 +35,7 @@ const Navbar = () => {
             </Button>
           </Link>
           <Button onClick={toggleColorMode}>
-            {colorMode === "light" ? "🌙" : "☀️"}
+            {colorMode === "light" ? "Dark" : "Light"}
           </Button>
         </HStack>
       </Flex>
