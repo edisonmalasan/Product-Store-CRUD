da10c4ae76dbd67ea2f5fbebea2715fa97a10a04
Eruki <malasanedison1@gmail.com>
2025-01-29 01:22:12 +0800

    slight changes from inputs

--- FrontEnd/src/pages/CreatePage.jsx
+++ FrontEnd/src/pages/CreatePage.jsx
@@ -30,31 +30,25 @@ const CreatePage = () => {
               name='name'
               value={newProduct.name}
               onChange={(e) => setNewProduct({ ...newProduct, name: e.target.value })}
-            >
-              // Product Name
-            </Input>
+            />
             <Input
               placeholder="Price"
               name='price'
               type='number'
               value={newProduct.price}
               onChange={(e) => setNewProduct({ ...newProduct, price: e.target.value })} 
-              >
-                // Price
-              </Input>
+              />
             <Input
               placeholder="Image URL"
               name='image'
               value={newProduct.image}
               onChange={(e) => setNewProduct({ ...newProduct, image: e.target.value })}
-            >
-              // Image
-            </Input>
+            />
           </VStack>
         </Box>
       </VStack>
     </Container>
-  )
-}
+  );
+};
 
-export default CreatePage
+export default CreatePage;
