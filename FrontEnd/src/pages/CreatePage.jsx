d2f6a0fe79c261ed91740937b62362e69ab16459
Eruki <malasanedison1@gmail.com>
2025-01-29 01:18:02 +0800

    added input with placeholder

--- FrontEnd/src/pages/CreatePage.jsx
+++ FrontEnd/src/pages/CreatePage.jsx
@@ -1,3 +1,4 @@
+import { Box, Container, Heading, useColorMode, useColorModeValue } from '@chakra-ui/react';
 import React from 'react'
 
 const CreatePage = () => {
@@ -8,9 +9,34 @@ const CreatePage = () => {
   });
 
   return (
-    <div>
-      CreatePage
-    </div>
+    <Container maxW={"container.sm"} px={4} >
+      <VStack 
+      spacing={8}
+      >
+        <Heading as={"h1"} size={"2xl"} textAlign={"center"} mb={8}>
+          Create New Product
+        </Heading>
+        <Box
+        w={"full"} 
+        bg={useColorModeValue("white", "gray.800")} 
+        p={6} rounded={"lg"} 
+        boxShadow={"lg"}
+        >
+          <VStack
+          spacing={4}
+          >
+            <Input
+              placeholder="Product Name"
+              name='name'
+              value={newProduct.name}
+              onChange={(e) => setNewProduct({ ...newProduct, name: e.target.value })}
+            >
+
+            </Input>
+          </VStack>
+        </Box>
+      </VStack>
+    </Container>
   )
 }
 
